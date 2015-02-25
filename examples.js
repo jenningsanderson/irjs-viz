f202fd8ef8ef6e45ff3b1a6e014815149d7d3748
Jennings Anderson <jennings.anderson@colorado.edu>
2015-02-24 18:02:14 -0700

    Better random data generation

--- examples.js
+++ examples.js
@@ -1,6 +1,23 @@
 var domout = require('./lib/domout'),
     viz = require('./lib/viz')
 
+
+function one_d_data(amount){
+  bars = []
+  for(var i=0; i<amount; i++){
+    bars.push(Math.floor(Math.random()*100)+1)
+  }
+  return bars
+}
+
+function two_d_data(amount){
+  data = []
+  for(var i=0; i<amount; i++){
+    data.push( [Math.floor((Math.random() * 375) + 1), Math.floor((Math.random() * 100) + 1)] )
+  }
+  return data
+}
+
 var data = [5, 10, 15, 20, 25, 30, 35, 50]
 
 domout.html('<h1>Circles</h1>')
@@ -8,19 +25,15 @@ domout.html('<h1>Circles</h1>')
 // note: use domout.svg for <svg> tags for them to be displayed properly
 domout.svg(viz.circles(data))
 
-bars = []
-for(var i=0; i<10; i++){
-  bars.push(Math.floor(Math.random()*100)+1)
-}
-
 domout.html('<h1>Barchart</h1>')
-domout.svg(viz.barchart(bars, {
+domout.svg(viz.barchart(one_d_data(10), {
     title: 'Bar Chart'
 }))
 
 domout.html('<h1>Line Graph</h1>')
-domout.svg(viz.linegraph(data, {
-  title: 'LineGraph Example'
+domout.svg(viz.linegraph(one_d_data(30), {
+  title: 'LineGraph Example',
+  width: 30
 }))
 
 domout.html('<h1>List</h1>')
@@ -38,14 +51,7 @@ domout.html(viz.json(data))
 domout.html('<h1>Table</h1>')
 domout.html(viz.table(data))
 
-data = []
-for(var i=0; i<50; i++){
-  data.push( [Math.floor((Math.random() * 375) + 1), Math.floor((Math.random() * 100) + 1)] )
-}
-
-console.log(data)
-
 domout.html('<h1>Scatter Plot</h1>')
-domout.svg(viz.scatterplot(data, {
+domout.svg(viz.scatterplot(two_d_data(50), {
   title: "A Scatter Plot from a 2d Array"
 }))
